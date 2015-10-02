f6529542a9c39ae3da92ff53b0cb010f07c82021
Roman Klimenko <romaklimenko@gmail.com>
2015-10-02 23:57:40 +0200

    get rid of require-text and timeline.json

--- main.js
+++ main.js
@@ -2,14 +2,13 @@
   require.config({
     paths: {
       "raphael": "./bower_components/raphael/raphael",
-      "text": "./bower_components/requirejs-text/text",
       "timeline": "./timeline"
     }
   });
 
-  require(["timeline", "text!./timeline.json"], (function(_this) {
-    return function(Timeline, text) {
-      return Timeline.render(document.getElementById('timeline'), JSON.parse(text));
+  require(["timeline"], (function(_this) {
+    return function(Timeline) {
+      return Timeline.render(document.getElementById('timeline'));
     };
   })(this));
 
